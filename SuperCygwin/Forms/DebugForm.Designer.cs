56219e30aa44f39b7a1db427f0b2e9a86f368857
ags131 <ashumann13@gmail.com>
2013-04-18 08:13:03 -0500

    Removed old testing code thats been implemented elsewhere.

--- SuperCygwin/Forms/DebugForm.Designer.cs
+++ SuperCygwin/Forms/DebugForm.Designer.cs
@@ -28,79 +28,38 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.radioCygwin = new System.Windows.Forms.RadioButton();
-            this.radioPutty = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
-            // button1
+            // label1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 85);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(97, 24);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Import";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Controls.Add(this.radioPutty);
-            this.groupBox1.Controls.Add(this.radioCygwin);
-            this.groupBox1.Location = new System.Drawing.Point(12, 12);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(97, 67);
-            this.groupBox1.TabIndex = 2;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "SSH Handler";
-            // 
-            // radioCygwin
-            // 
-            this.radioCygwin.AutoSize = true;
-            this.radioCygwin.Location = new System.Drawing.Point(12, 19);
-            this.radioCygwin.Name = "radioCygwin";
-            this.radioCygwin.Size = new System.Drawing.Size(59, 17);
-            this.radioCygwin.TabIndex = 3;
-            this.radioCygwin.TabStop = true;
-            this.radioCygwin.Text = "Cygwin";
-            this.radioCygwin.UseVisualStyleBackColor = true;
-            // 
-            // radioPutty
-            // 
-            this.radioPutty.AutoSize = true;
-            this.radioPutty.Location = new System.Drawing.Point(12, 42);
-            this.radioPutty.Name = "radioPutty";
-            this.radioPutty.Size = new System.Drawing.Size(49, 17);
-            this.radioPutty.TabIndex = 3;
-            this.radioPutty.TabStop = true;
-            this.radioPutty.Text = "Putty";
-            this.radioPutty.UseVisualStyleBackColor = true;
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(293, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Nothing here atm :)";
             // 
             // DebugForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(706, 262);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "DebugForm";
             this.Text = "DebugForm";
             this.Load += new System.EventHandler(this.DebugForm_Load);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.RadioButton radioPutty;
-        private System.Windows.Forms.RadioButton radioCygwin;
+        private System.Windows.Forms.Label label1;
+
 
     }
 }
